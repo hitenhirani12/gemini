b0ace5cb1453ed7fce3412464dd46f1e010c5985
hitenhirani12 <162007095+hitenhirani12@users.noreply.github.com>
2025-08-07 23:40:08 -0600

    commit

--- src/components/Main/ShinyText.jsx
+++ src/components/Main/ShinyText.jsx
@@ -1,18 +1,16 @@
-// src/components/Main/ShinyText.jsx
-import React from 'react';
 import './ShinyText.css';
 
 const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
-  const animationDuration = `${speed}s`;
+  const animationDuration = `${speed}s`;
 
-  return (
-    <span
-      className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`}
-      style={{ animationDuration }}
-    >
-      {text}
-    </span>
-  );
+  return (
+    <div
+      className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`}
+      style={{ animationDuration }}
+    >
+      {text}
+    </div>
+  );
 };
 
 export default ShinyText;
